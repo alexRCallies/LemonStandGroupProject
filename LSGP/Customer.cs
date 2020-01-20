456c14c7d13f5e88182d9011caaafebbdb581511
Nicolas Timm <nntimm1@gmail.com>
2020-01-20 07:23:31 -0600

    building customer class

--- LSGP/Customer.cs
+++ LSGP/Customer.cs
@@ -12,7 +12,8 @@ namespace LSGP
 
         public string name;
         public int age;
-
+        List<string> customer = new List<string>() { "David", "Mark", "Sasha", "Steven", "Ryan", "Leah", "Alex", "Nick", "Mike", "Paul", "Lisa", "Yolanda", "Madonna", "Richard", "Aaron", "Peggy", "Carol", "Lauri", "Sonya", "Suzy", "Marion", "Cliff", "Donald", "Fino", "Mary", "Erica", "Zeek", "Liv" };
+        Random random = new Random();
 
 
         // Constructors
@@ -22,6 +23,13 @@ namespace LSGP
 
         // Member Methods
 
+        //public void CustomerSelection()
+        //{
+        //    customer = new List<string>() { "David", "Mark", "Sasha", "Steven", "Ryan", "Leah", "Alex", "Nick", "Mike", "Paul", "Lisa", "Yolanda", "Madonna", "Richard", "Aaron", "Peggy", "Carol", "Lauri", "Sonya", "Suzy", "Marion", "Cliff", "Donald", "Fino", "Mary", "Erica", "Zeek", "Liv" };
+        //    int index = random.Next(customer.Count);
+        //    newCustomer = customer[index];
+        //    Console.WriteLine(newCustomer);
 
+        //}
     }
 }
