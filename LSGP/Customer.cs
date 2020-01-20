958d4e29108219002de878bbbb841d8d71b9b637
Nicolas Timm <nntimm1@gmail.com>
2020-01-20 08:25:08 -0600

    build master method, added instructions w/ user validation

--- LSGP/Customer.cs
+++ LSGP/Customer.cs
@@ -23,13 +23,6 @@ namespace LSGP
 
         // Member Methods
 
-        //public void CustomerSelection()
-        //{
-        //    customer = new List<string>() { "David", "Mark", "Sasha", "Steven", "Ryan", "Leah", "Alex", "Nick", "Mike", "Paul", "Lisa", "Yolanda", "Madonna", "Richard", "Aaron", "Peggy", "Carol", "Lauri", "Sonya", "Suzy", "Marion", "Cliff", "Donald", "Fino", "Mary", "Erica", "Zeek", "Liv" };
-        //    int index = random.Next(customer.Count);
-        //    newCustomer = customer[index];
-        //    Console.WriteLine(newCustomer);
 
-        //}
     }
 }
--- LSGP/Game.cs
+++ LSGP/Game.cs
@@ -17,8 +17,38 @@ namespace LSGP
             this.store = new Store(player);
         }
 
+        public void RunGame() //Master Method
+        {
+            Instructions();
+            FindPlayerName();
+            PlayerChoice();
+        }
 
-
+        public void Instructions()
+        {
+            Console.WriteLine("Instructions");
+            Console.WriteLine("\n Your goal is to make as much money as you can in \n 7, 14, or 30 days by " +
+                "selling lemonade at your lemonade \n  stand.");
+            Console.WriteLine("\n\n Buy cups, lemons, sugar, and ice cubes, then set\n your recipe based on " +
+                "the weather and conditions. Start\n with the basic recipe, but try to vary the recipe \n" +
+                " and see if you can do better. Lastly, set your price \n and sell your lemonade at the stand." +
+                " Try changing \n up the price based on the weather conditions as well.");
+            Console.WriteLine("\n\n At the end of the game, you'll see how much money\n you made. Write it down and " +
+                "play again\n to try and beat your score!");
+            Console.WriteLine("\nReady to play?  Yes or No" );
+            string confirmation = Console.ReadLine();
+            if ((confirmation == "no")||(confirmation == "No")||(confirmation == "NO")||(confirmation == "n")
+                ||(confirmation == "N")||(confirmation=="nope")||(confirmation=="Nope")||(confirmation == "NOPE")||
+                (confirmation == "NOpe")||(confirmation == "NOPe")||(confirmation == "nOpe"))
+            {
+                Console.WriteLine("Alright, lets go over the instructions again:");
+                RunGame();
+            }
+            else
+            {
+                Console.WriteLine("Great! Lets get started!");
+            }
+        }
         public string FindPlayerName()
         {
             Console.WriteLine("What is your name?");
