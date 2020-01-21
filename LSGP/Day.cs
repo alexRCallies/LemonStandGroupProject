d1c147778e4cccc262b4a1dceb2bfded3c1ba96e
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-01-21 10:41:51 -0600

    customer condtions with price and sweetness

--- LSGP/Day.cs
+++ LSGP/Day.cs
@@ -37,21 +37,21 @@ namespace LSGP
         public Day(Player player)
         {
             this.player = player;
-            this.aaron = new Customer("A-a-ron", 14, 13, 15, 12, 15, 5);
-            this.alex = new Customer("Alex", 21, 12, 14, 15, 12, 4);
-            this.david = new Customer("David", 32, 15, 14, 12, 14, 6);
-            this.leah = new Customer("Leah", 15, 15, 5, 12, 15, 4);
-            this.lisa = new Customer("Lisa", 28, 10, 15, 12, 17, 5);
-            this.madonna = new Customer("Madonna", 55, 12, 16, 12, 14, 3);
-            this.mark = new Customer("Mark", 24, 16, 10, 12, 14, 5);
-            this.mike = new Customer("Mike", 33, 14, 15, 12, 17, 4);
-            this.nick = new Customer("Nick", 41, 10, 16, 12, 16, 5);
-            this.paul = new Customer("Paul", 45, 12, 14, 12, 14, 5);
-            this.richard = new Customer("Richard", 25, 13, 17, 12, 17, 6);
-            this.ryan = new Customer("Ryan", 70, 13, 16, 12, 16, 8);
-            this.sasha = new Customer("Sashe", 37, 13, 13, 12, 15, 4);
-            this.steven = new Customer("Steven", 43, 15, 15, 12, 15, 4);
-            this.yolanda = new Customer("Yolanda", 53, 10, 15, 12, 15, 5);
+            this.aaron = new Customer("A-a-ron", 14, 65, 50, 60, 75, 50);
+            this.alex = new Customer("Alex", 21, 60, 60, 75, 60, 40);
+            this.david = new Customer("David", 32, 50, 65, 60, 70, 60);
+            this.leah = new Customer("Leah", 15, 55, 35, 60, 75, 40);
+            this.lisa = new Customer("Lisa", 28, 45, 75, 60, 85, 50);
+            this.madonna = new Customer("Madonna", 55, 60, 70, 60, 65, 30);
+            this.mark = new Customer("Mark", 24, 45, 50, 60, 65, 50);
+            this.mike = new Customer("Mike", 33, 65, 65, 60, 60, 40);
+            this.nick = new Customer("Nick", 41, 50, 55, 60, 50, 50);
+            this.paul = new Customer("Paul", 45, 60, 65, 60, 50, 50);
+            this.richard = new Customer("Richard", 25, 65, 80, 60, 65, 60);
+            this.ryan = new Customer("Ryan", 70, 55, 50, 60, 45, 80);
+            this.sasha = new Customer("Sashe", 37, 45, 45, 60, 45, 40);
+            this.steven = new Customer("Steven", 43, 70, 70, 60, 50, 40);
+            this.yolanda = new Customer("Yolanda", 53, 50, 75, 60, 60, 50);
             customers.Add(aaron);
             customers.Add(alex);
             customers.Add(david);
@@ -106,12 +106,12 @@ namespace LSGP
             for (int i = 0; i<remainingCustomers.Count;i++)
                 
                 {
-                //Random randomBasedOnPrice = new Random();
-                int priceBuy = remainingCustomers[0].rng1.Next(1, 21);
-               // Random randomBasedOnSweetness = new Random();
-                int sweetBuy = remainingCustomers[0].rng1.Next(1, 21);
-               // Random randomBasedOnColdness = new Random();
-                int coldBuy = remainingCustomers[0].rng1.Next(1, 21);
+                
+                int priceBuy = remainingCustomers[0].rng1.Next(1, 101);
+              
+                int sweetBuy = remainingCustomers[0].rng1.Next(1, 101);
+             
+                int coldBuy = remainingCustomers[0].rng1.Next(1, 101);
                 if (priceBuy <= remainingCustomers[i].chanceToBuyprice)
                 {
 
@@ -168,7 +168,7 @@ namespace LSGP
             for (int i = 0; i<customers.Count;i++)
             { 
             Random buyAgain = new Random();
-            int again = customers[0].rng1.Next(1, 11);
+            int again = customers[0].rng1.Next(1, 101);
             if (again <= customers[i].chanceToBuyAgain)
             {
                     remainingCustomers.Add(customers[i]);
@@ -197,27 +197,65 @@ namespace LSGP
                 if (weather.condition == weather.weatherConditions[3])
 
                 {
-                    customers[i].chanceToGoToStand += 2;
-                    customers[i].chanceToBuyColdLevel -= 3;
+                    customers[i].chanceToGoToStand += 20;
+                   
 
                 }
 
                 else if (weather.condition == weather.weatherConditions[1])
 
                 {
-                    customers[i].chanceToGoToStand -= 3;
-                    customers[i].chanceToBuyColdLevel += 5;
+                    customers[i].chanceToGoToStand -= 15;
+                    
                 }
 
                 else if (weather.condition == weather.weatherConditions[2])
 
                 {
-                    customers[i].chanceToGoToStand -= 10;
-                    customers[i].chanceToBuyColdLevel += 10;
+                    customers[i].chanceToGoToStand -= 25;
+                    
                 }
             }
         }
-
+        public void CheckOtherCondition()
+        {
+            for(int i = 0; i < remainingCustomers.Count; i++)
+            {
+                if (player.recipe.pricePerCup >= 5)
+                {
+                    remainingCustomers[i].chanceToBuyprice -= 15;
+                }
+                else if (player.recipe.pricePerCup == 2)
+                {
+                    remainingCustomers[i].chanceToBuyprice += 10;
+                }
+                else if (player.recipe.pricePerCup == 1)
+                {
+                    remainingCustomers[i].chanceToBuyprice += 25;
+                }
+                else if (player.recipe.pricePerCup == 4)
+                {
+                    remainingCustomers[i].chanceToBuyprice -= 10;
+                }
+                if(player.inventory.pitchers[0].sweetLevel == 5)
+                {
+                    remainingCustomers[i].chanceToBuySweetLevel += 100;
+                }
+                else if(player.inventory.pitchers[0].sweetLevel == 4)
+                {
+                    remainingCustomers[i].chanceToBuySweetLevel += 40;
+                }
+                else if(player.inventory.pitchers[0].sweetLevel == 2)
+                {
+                    remainingCustomers[i].chanceToBuySweetLevel -= 15;
+                }
+                else if(player.inventory.pitchers[0].sweetLevel == 1)
+                {
+                    remainingCustomers[i].chanceToBuySweetLevel -= 25;
+                }
+                
+            }
+        }
         public void MasterCustomerBuyLemonade()
         {
             
